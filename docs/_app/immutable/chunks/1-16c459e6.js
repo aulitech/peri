26d092382a6e38b3c68bf8b040363b2f375c6ee4
Spero Koulouras <spero@auli.tech>
2022-09-09 14:23:56 -0700

    Typo

--- docs/_app/immutable/chunks/1-16c459e6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-6e6f78d1.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1-9738add0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-16a9c858.js";export{t as component};
