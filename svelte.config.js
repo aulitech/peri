8924e571bc3b00129d9b4cf61bdfa3a7b84e3b3e
Spero Koulouras <spero@auli.tech>
2022-09-09 18:00:24 -0700

    Again

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
         }),
         paths: {
             // change below to your repo name
-            base: dev ? "" : "/tw-theme",
+            base: dev ? "" : "/aulitech/tw-theme/docs",
         }
     }
 };
