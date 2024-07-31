19cdd15e6fcc47004e2d1f44f8d984151be8b1b9
Logan Somoza <logansomoza@gmail.com>
2024-07-31 14:01:53 -0700

    added changes

--- docs/_app/immutable/chunks/1-a1a5ab9c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-f0b31dec.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1-f64faa6c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-1e3987f6.js";export{t as component};
