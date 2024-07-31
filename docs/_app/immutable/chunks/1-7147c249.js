f4345fb3c9c29a1f2224c1e86e74bceeb9bed94c
Logan Somoza <logansomoza@gmail.com>
2024-07-31 10:44:36 -0700

    Edited changes to allow weighting to work again
    Did remove the edge case for database initialization

---
+++ docs/_app/immutable/chunks/1-7147c249.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-012a8b79.js";export{t as component};
--- docs/_app/immutable/chunks/2-e8c6fad6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-7f660c33.js";export{t as component};
