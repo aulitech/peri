bb6e7f725f1d99dd8c3b5c1a3e4617cdbbfe9048
Logan Somoza <logansomoza@gmail.com>
2024-08-04 23:49:22 -0700

    Deleted unused code in pause.svelte

--- docs/_app/immutable/chunks/2-a3c0bdf4.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-5d4b102a.js";export{t as component};
--- src/components/pause.svelte
+++ src/components/pause.svelte
@@ -1,16 +1,9 @@
 <script>
-    import { writable } from 'svelte/store';
 	export let Pause;
-    export const isPaused = writable(false);
-    function togglePause() {
-        //console.log('paused clicked');
-        //isPaused.update(currentValue => !currentValue);
-    }
 </script>
 
 <svg class="w-8 h-8 {Pause.class}" 
-    viewBox="0 0 384 512"
-    on:mouseenter={() => togglePause()}> 
+    viewBox="0 0 384 512">
 	<path 
 		fill="currentColor"
         d="M144 85.12h20v341.76h-65V85.12zm128 0h20v341.76h-65V85.12z"
